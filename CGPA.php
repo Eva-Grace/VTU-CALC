b77d072df24a6f0e7378dc2e76def5ee9b6f030e
vishnukoneri <99kgvishnu@gmail.com>
2021-09-23 18:23:35 +0530

    cgpa file added

---
+++ CGPA.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VTU CALCULATOR</title>
+</head>
+<body>
+<h2>CGPA Calculator</h2>
+<!-- <form action="CGPA.php"> -->
+    <label>SELECT YOUR SEM: </label></br>
+    <label>1st Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem1"><button class="inputsidebar">/10</button></br>
+    <label>2nd Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem2"><button class="inputsidebar">/10</button></br>
+    <label>3rd Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem3"><button class="inputsidebar">/10</button></br>
+    <label>4th Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem4"><button class="inputsidebar">/10</button></br>
+    <label>5th Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem5"><button class="inputsidebar">/10</button></br>
+    <label>6th Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem6"><button class="inputsidebar">/10</button></br>
+    <label>7th Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem7"><button class="inputsidebar">/10</button></br>
+    <label>8th Sem: </label><input type="number" min="0" max="10" placeholder="SGPA" ng-model="sem8"><button class="inputsidebar">/10</button></br>
+
+<!-- </form> -->
+</body>
+</html>
