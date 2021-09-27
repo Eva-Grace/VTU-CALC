01a609b9864d79de7c840fcafd344bba74ff5b59
vishnukoneri <99kgvishnu@gmail.com>
2021-09-27 21:29:44 +0530

    styling for signin and signup

--- signin.php
+++ signin.php
@@ -7,25 +7,23 @@
 <link rel="stylesheet" type="text/css" href="externalStyle.css">
 <body>
   	<h2>Signin</h2>
-
-	<div class="container"> 
   <form method="post" action="signin.php">
   	<?php include('errors.php'); ?>
-  	<div class="input-group">
+  	<div class="input-group1">
 	  	<label>Email</label>
   	  	<input type="email" name="email" value="<?php echo $email; ?>" placeholder="Enter Your Email">
   	</div>
-  	<div class="input-group">
+  	<div class="input-group1">
   		<label>Password</label>
   		<input type="password" name="password" placeholder="Enter Your Password">
   	</div>
-  	<div class="input-group">
+  	<div class="input-group1">
   		<button type="submit" class="btn" name="login_user">Login</button>
   	</div>
   	<p>
   		Not yet a member? <a href="signup.php">Sign up</a>
   	</p>
   </form>
-  </div>
+
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -11,59 +11,34 @@
   </div>
 	
   <form method="post" action="signup.php">
-	<?php include('errors.php'); ?>
-	<div class = "dropdown">
-		<table class="input">
-			<tr>
-				<td>
-					Username :
-				</td>
-				<td>
-					<input type="text" name="username" value="<?php echo $username; ?>" placeholder="Enter Your Name" required>
-				</td>	
-			</tr>
-
-			<tr>	
-				<td>
-					<label>Email</label>
-				</td>
-				<td>
-					<input type="email" name="email" value="<?php echo $email; ?>" placeholder="Enter Your Email" required>
-				</td>
-			</tr>
-
-			<tr>
-				<td>
-					<label>Password</label>
-				</td>	
-				<td>
-					<input type="password" name="password_1" placeholder="Enter Your Password" required>				
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<label>Confirm password</label>
-				</td>
-				<td>
-					<input type="password" name="password_2" placeholder="Enter Your Password" required>
-				</td>
-			</tr>
-			<tr>
-				<td>				
-					<label>Phone</label>
-				</td>
-				<td>
-					<input type="tel" name="phone" placeholder="1234567890" pattern="[0-9]{10}" required>				
-				</td>
-			</tr>
-		</table>
-		</div>
-		
-			
-			<div class="container">
-				<button type="submit" class="btn" name="reg_user">Register</button>
-			</div>
-			<div class="container">
+  <?php include('errors.php'); ?>
+  	<div class="input-group1">
+  	  	<label>Username</label>
+  	  	<input type="text" name="username" value="<?php echo $username; ?>" placeholder="Enter Your Name">
+  	</div>
+  	<div class="input-group1">
+  	  	<label>Email</label>
+  		<input type="email" name="email" value="<?php echo $email; ?>" placeholder="Enter Your Email">
+  	</div>
+  	<div class="input-group1">
+  	  	<label>Password</label>
+  	  	<input type="password" name="password_1" placeholder="Enter Your Password">
+  	</div>
+  	<div class="input-group1">
+  	  	<label>Confirm password</label>
+  	  	<input type="password" name="password_2" placeholder="Enter Your Password">
+  	</div>
+	<div class="input-group1">
+  	  	<label>Phone</label>
+  	  	<input type="tel" name="phone" placeholder="1234567890" pattern="[0-9]{10}">
+  	</div>
+	<div class="input-group1">
+  	  	<label>Address</label>
+  	  	<input type="text" name="address" value="<?php echo $address; ?>" placeholder="Enter Your Address">
+  	</div>
+  	<div class="input-group1">
+  	  	<button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
 				<p>
 					Already a member? <a href="signin.php">Sign in</a>
 				</p>
