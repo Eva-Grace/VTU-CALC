8c9f40dc8f81311c276acaa5e1ac5974a16efa47
vishnukoneri <99kgvishnu@gmail.com>
2021-09-27 23:29:50 +0530

    address part removed

--- signup.php
+++ signup.php
@@ -32,10 +32,6 @@
   	  	<label>Phone</label>
   	  	<input type="tel" name="phone" placeholder="1234567890" pattern="[0-9]{10}">
   	</div>
-	<div class="input-group1">
-  	  	<label>Address</label>
-  	  	<input type="text" name="address" value="<?php echo $address; ?>" placeholder="Enter Your Address">
-  	</div>
   	<div class="input-group1">
   	  	<button type="submit" class="btn" name="reg_user">Register</button>
   	</div>
