3d78ca53714e16ed998df1cb7d87544e4d389112
vishnukoneri <99kgvishnu@gmail.com>
2021-09-28 11:12:29 +0530

    coorected redirection error from signup to calc

--- server.php
+++ server.php
@@ -55,7 +55,7 @@ if (isset($_POST['reg_user'])) {
     
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-  	header('location: calc.html');
+  	header('location: calc.php');
   }
 }
 
